5a0863bf61eba73391bfb208dca056ed06170a77
Clara <144264170+clara-tyman@users.noreply.github.com>
2024-05-23 03:18:31 +0000

    everythign

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Clara Tyman
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+'use strict'
+
+/**
+ * Check service worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICD2O-Unit-6-02-clara-tyman/sw.js", {
+    scope: "/ICD2O-Unit-6-02-clara-tyman/",
+  })
+}
+
+/**
+ * This function updates the cookie count.
+ */
+function updateCookieCount() {
+
+  // input and process
+  if (localStorage.updateCookieCount) {
+    localStorage.updateCookieCount = Number(localStorage.updateCookieCount) + 1
+  } else {
+    localStorage.updateCookieCount = 1
+  }
+
+  // output
+  document.getElementById("answer").innerHTML = "The amount of times clicked: " + localStorage.updateCookieCount
+}
